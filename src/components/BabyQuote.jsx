e508a0b2f4000f26b92c9358921508749e36f10b
ziauddinziya07 <ziauddinziya07@gmail.com>
2022-07-12 18:56:46 +0530

    second commit

--- src/components/BabyQuote.jsx
+++ src/components/BabyQuote.jsx
@@ -9,19 +9,19 @@ function BabyQuote() {
             <div className="baby-quote-wrapper">
                 <div className="inner-content">
                     <h3>
-                        My Mom, Dad and Grand Parents have decided to Name me and
-                        preform my Aqeeqah and we want you and your Family to
-                        share our happiness together. So plese be there to bless
-                        me when I get named ...
+                        My Mom, Dad and Grand Parents have decided to Name me
+                        and preform my Aqeeqah and we want you and your Family
+                        to share our happiness together. So plese be there to
+                        bless me when I get named ...
                     </h3>
                     <div className="right-align">
                         <h3>
                             With Love
-                            <FontAwesomeIcon
-                                icon={faHeart}
-                                className="icon"
-                                size="sm"
-                            />
+                                <FontAwesomeIcon
+                                    icon={faHeart}
+                                    className="icon"
+                                    size="sm"
+                                />
                         </h3>
                         <h2 className="name">--- Noorain Raza</h2>
                     </div>
